5dd711f9627749af6054b80f0d07a102966f3ffd
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-08-22 21:35:17 +0300

    Made migrations

--- database/migrations/2023_08_22_182807_create_collections_table.php
+++ database/migrations/2023_08_22_182807_create_collections_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->float('target_amount');
+            $table->string('link');
             $table->timestamps();
         });
     }
--- database/migrations/2023_08_22_182833_create_contributors_table.php
+++ database/migrations/2023_08_22_182833_create_contributors_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('contributors', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('collection_id')->constrained()->cascadeOnDelete();
+            $table->string('user_name');
+            $table->float('amount');
             $table->timestamps();
         });
     }

3a8096735f658b1d6f25611ae810fd05abac1a89
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-08-27 15:40:45 +0300

    feat(collection): made a procedure for filtering

---
+++ database/migrations/2023_08_27_094142_create_stored_procedures_for_filters.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('DROP PROCEDURE IF EXISTS contributions_by_collection;');
+        DB::statement("
+            CREATE PROCEDURE contributions_by_collection()
+            BEGIN
+                CREATE TEMPORARY TABLE contribution_sum
+                SELECT collection_id, SUM(amount) AS total 
+                FROM contributors 
+                GROUP BY collection_id;
+
+                SELECT id, 
+                    title, 
+                    description, 
+                    target_amount, 
+                    link, 
+                    total, 
+                    (target_amount - total) AS sum_left 
+                FROM contribution_sum INNER JOIN collections 
+                    ON contribution_sum.collection_id = collections.id;
+
+                DROP TEMPORARY TABLE IF EXISTS contribution_sum;
+            END;");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP PROCEDURE IF EXISTS contributions_by_collection;');
+    }
+};

3b3fcbbda71bb201a58f9717f164c26ae35c8136
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-08-28 02:56:42 +0300

    refactor(command): wrote comments for command

--- app/Commands/Command.php
+++ app/Commands/Command.php
@@ -2,15 +2,32 @@
 
 namespace App\Commands;
 
+/*
+* Base class for all commands
+* Implements the Command Pattern
+*/
 abstract class Command {
+    /**
+     * Define in subclass
+     * and put the logic of a command
+     */
     public abstract function execute(): Result;
 
+    /**
+     * Called to create instance 
+     * of subclass with parameterns 
+     * in constructor and execute it
+     */
     public static function call(...$args): Result
     {
         $instance = self::create(...$args);
         return $instance->execute();
     }
 
+    /**
+     * Creates instance of subclass
+     * with arguments in constructor
+     */
     private static function create(...$args): self
     {
         return new static(...$args);

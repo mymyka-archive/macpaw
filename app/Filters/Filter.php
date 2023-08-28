700640305c7d11697760327ec1e742e1460ee976
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-08-28 03:01:46 +0300

    refactor(filter): wrote comments for filter

--- app/Filters/Filter.php
+++ app/Filters/Filter.php
@@ -4,16 +4,26 @@ namespace App\Filters;
 
 use Illuminate\Database\Query\Builder;
 
+/**
+ * Base class for all filters
+ * Implements the Chain Of Responsobility Pattern
+ */
 abstract class Filter
 {
     protected ?Filter $nextFilter = null;
 
+    /**
+     * Called to select nex filter in the chain
+     */
     public function then(Filter $filter): Filter
     {
         $this->nextFilter = $filter;
         return $filter;
     }
 
+    /**
+     * Called to call next filter in the chain
+     */
     public function nextStep(Builder $data): ?Builder
     {
         if ($this->nextFilter) {
@@ -22,5 +32,9 @@ abstract class Filter
         return $data;
     }
 
+    /**
+     * Define in subclasses
+     * and put the logic of a filter
+     */
     public abstract function filter(Builder $data): Builder;
 }
